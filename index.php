dfd4dda0cba2927c2d5395c2cc6740b7100d1448
carlosmirabal <100005846.joan23@fje.edu>
2020-10-05 19:19:33 +0200

    Subida 2

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Secular+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+    <div class="hero-image">
+            d
+        </div>
+        <div class="nav">
+            <ul>
+                <li><a class="active" href="#home">Inicio</a></li>
+                <li><a href="#news">Juegos</a></li>
+                <li><a href="#contact">Estrenos</a></li>
+                <li><a href="#about">Noticias</a></li>
+            </ul>
+        </div>
+    </header>
+    <p>Pero ¿de verdad es así? La respuesta tiene que ser negativa. La política no se hace en las urnas. El sistema de partido hegemónico que impera en México desde 1929 determina que la verdadera elección de diputados haya ocurrido en febrero, probablemente cuando los diversos intereses que es aglutinan en el PRI acordaron las candidaturas, en un proceso que sólo es formalmente interior, pero que por la naturaleza del partido es el que verdaderamente decide la composición de la Cámara.
+
+Fuente: https://www.ejemplode.com/12-clases_de_espanol/362-ejemplo_de_parrafo.html#ixzz6a1BLmYT0</p>
+
+</body>
+</html>
