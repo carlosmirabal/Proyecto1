eb9e7a7f25b6e11dfc08890f15e3a188afededb0
carlosmirabal <100005846.joan23@fje.edu>
2020-10-14 16:02:35 +0200

    Casi Acabado

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="css/style2.css" type="text/css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
@@ -93,10 +94,10 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-4">
-                <img src="img/cyberpunk.jpg" alt="">
+                <img src="img/games.jpg" alt="">
             </div>
             <div class="col-sm-6">
-                <h2>CyberPunk Vuelte A Retrasarse</h2>
+                <h2>Inicio</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ullamcorper ultrices diam, ut interdum augue ullamcorper eu. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean pellentesque pellentesque magna a mattis. Nulla nunc libero, ultrices efficitur est non, posuere mattis felis. Donec lobortis ex ut felis tincidunt, eu eleifend elit aliquam. Praesent tempus libero sed lacus pulvinar, a finibus nunc ultrices. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce ipsum ligula, malesuada non sapien quis, suscipit accumsan nulla. Pellentesque sollicitudin arcu ac tortor posuere lobortis. Ut nec dignissim tellus. In vitae leo a augue egestas ornare.</p>
             </div>
         </div>
@@ -107,12 +108,118 @@
         <img src="img/banner3.jpg" alt="Banner2" class="img-fluid">
             <div class="text-block text-center">
                 <h2>Toda La Actualidad De Los Videojuegos</h2>                
-                <button type="button" class="btn btn-outline-danger btn-lg waves-effect">Próximos Lanzamientos</button>
+                <button type="button" class="btn btn-outline-danger btn-lg waves-effect">Entrar</button>
             </div>
     </div>
 
-    <br>
-    <br>
-    
+    <!-- noticias -->
+    <h1>Noticias</h1>
+    <div class="card-deck">
+    <div class="card">
+        <img src="img/cyberpunk.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Se retrasa el lanzamiento</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <p class="card-text"><small class="text-muted">10/10/2020</small></p>
+            <button type="button" class="btn btn-outline-danger btn-lg waves-effect">Leer más</button>
+        </div>
+    </div>
+    <div class="card">
+        <img src="img/coldwar.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Beta abierta del nuevo COD</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <button type="button" class="btn btn-outline-danger btn-lg waves-effect">Leer más</button>
+        </div>
+    </div>
+    <div class="card">
+        <img src="img/valorant.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Valorant Acto 3</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <button type="button" class="btn btn-outline-danger btn-lg waves-effect">Leer más</button>
+        </div>
+    </div>
+    </div>
+
+        <!-- footer -->
+        <footer>
+            <div class="container-fluid bg-footer">
+                <div class="container">
+
+                    <!-- footer menus -->
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h2>VIDEOJUEGOS</h2>
+
+                        </div>
+                        <div class="col-md-3 ">
+                            <h3>Acceso Rapido</h3>
+                            <ul class="list-unstyled lista">
+                                <li class="lista">
+                                    <a href="#">
+                                        Inicio
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="">
+                                        Noticias
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="">
+                                        Lanzamientos
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="">
+                                        Juegos
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-3 text-center">
+                            <h3>Social</h3>
+                            <ul class="list-unstyled ">
+                                <li>
+                                    <h3>
+                                     <a  class="fa fa-facebook"></a>
+                                    </h3>
+                                </li>
+                                <li>
+                                    <h3>
+                                        <a  class="fa fa-twitter" ></a>
+                                    </h3>
+                                </li>
+                                <li>
+                                    <h3>
+                                        <a  class="fa fa-google" ></a>
+                                    </h3>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- end footer menus -->
+
+                    <hr>
+
+                </div>
+            </div>
+
+            <!-- sub footer -->
+            <div class="container-fluid bg-copyright">
+                <div class="container text-center">
+                    <p class="copyright">
+                        &copy; 2020 VideoJuegos. Todos los derechos reservados
+                    </p>
+                </div>
+            </div>
+            <!-- end sub footer -->
+
+        </footer>
+        <!-- end footer -->
+
 </body>
 </html>
